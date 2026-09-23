e526e522381a5a1542b6fc3ecb6544c5c0306545
Vivek <vivek.etc@proton.me>
2026-09-24 02:04:23 +0530

    add favicon icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.ico';
+	import appleTouchIcon from '$lib/assets/apple-touch-icon.png';
+	import favicon32 from '$lib/assets/favicon-32x32.png';
+	import favicon16 from '$lib/assets/favicon-16x16.png';
+	import sitemap from '$lib/assets/site.webmanifest';
 
 	let { children } = $props();
 </script>
 
 <svelte:head
 	><link rel="icon" href={favicon} />
+	<link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
+	<link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
+	<link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
+	<link rel="manifest" href={sitemap} />
 	<style>
 		@import url('https://fonts.googleapis.com/css2?family=Carter+One&family=Roboto+Slab:wght@100..900&display=swap');
 	</style>
